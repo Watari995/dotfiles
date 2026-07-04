f85fdef2b095427983829ba0de050b3e7379ab77
Wataru Ichimura <wataru.ichimura@soda-inc.jp>
2026-07-04 12:10:41 +0900

    feat: manage Homebrew casks with nix-darwin

--- nix/darwin/homebrew.nix
+++ nix/darwin/homebrew.nix
@@ -1,6 +1,26 @@
 {
   homebrew = {
     enable = true;
+
+    casks = [
+      "codex"
+      "docker-desktop"
+      "dotnet-sdk"
+      "flutter"
+      "font-blex-mono-nerd-font"
+      "font-hackgen-nerd"
+      "font-jetbrains-mono"
+      "font-jetbrains-mono-nerd-font"
+      "gcloud-cli"
+      "hammerspoon"
+      "maestro"
+      "ngrok"
+      "raspberry-pi-imager"
+      "tailscale-app"
+      "visual-studio-code"
+      "wezterm@nightly"
+    ];
+
     onActivation = {
       autoUpdate = false;
       cleanup = "none";
