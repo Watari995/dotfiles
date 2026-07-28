4e161e7934ad45dea75f29648c4a28fb33ae4913
Wataru Ichimura <wataru.ichimura@soda-inc.jp>
2026-07-28 11:03:48 +0900

    Remove Snacks dashboard

--- nvim/lua/ichimurawataru/plugins/snacks.lua
+++ nvim/lua/ichimurawataru/plugins/snacks.lua
@@ -354,14 +354,6 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    dashboard = {
-      enabled = true,
-      sections = {
-        { section = "header" },
-        { section = "keys", gap = 1, padding = 1 },
-        { section = "startup" },
-      },
-    },
     explorer = {
       enabled = true,
       replace_netrw = true,
